9afdf5324532f4fa8eb6ea4e689d4b8fb4f03e9c
steelvelveteen <joeyvico@gmail.com>
2020-09-11 09:02:34 +0930

    Setting/modifying state with setState and callback

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 import Counter from "./Counter";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
       <Counter />
     </div>
   );
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 
 interface Props {
+  //   another: string;
+}
+interface State {
   count: number;
 }
 
-class Counter extends Component<State> {
+class Counter extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
@@ -12,10 +15,18 @@ class Counter extends Component<State> {
       count: 0,
     };
   }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 }, () => {
+      console.log("Callback value", this.state.count);
+    });
+  };
+
   render() {
     return (
       <div>
         <div>Count - {this.state.count}</div>
+        <button onClick={() => this.increment()}>Increment</button>
       </div>
     );
   }
